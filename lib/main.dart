284dbeac4e2e971c1900c5a07918b7617e21b1b7
unknown <jonnpuga1049@gmail.com>
2023-04-08 00:13:06 -0500

    Mensaje descriptivo de los cambios realizados

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,74 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'Paginas/bienvenida.dart';
+import 'Paginas/pagina_principal.dart';
+
+// La función main() que es la función de entrada de la aplicación de Flutter
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Se asegura que los widgets de Flutter estén correctamente inicializados
+  //await Firebase.initializeApp();
+
+  runApp(
+      PaginaBase()); // Se ejecuta la aplicación, pasando como argumento una instancia de la clase PaginaBase
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+// ignore: use_key_in_widget_constructors
+class PaginaBase extends StatelessWidget {
+  // Se define la estructura de la página principal de la app
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  // ignore: unnecessary_new, prefer_const_constructors
+  final storage = new FlutterSecureStorage();
+  Future<bool> checkLoginStatus() async {
+    // Se incluye una función checkLoginStatus para verificar que un usuario ha iniciado sesión
+    String? value = await storage.read(key: "uid");
+    if (value == null) {
+      return false;
+    }
+    return true;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for Errors
+          if (snapshot.hasError) {
+            // ignore: avoid_print
+            print("Algo salió mal");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // ignore: prefer_const_constructors
+            return Center(child: CircularProgressIndicator());
+          }
+          return MaterialApp(
+              title: 'Flutter Firebase EMail Password Auth',
+              theme: ThemeData(
+                primarySwatch: Colors.deepPurple,
+              ),
+              debugShowCheckedModeBanner: false,
+              home: FutureBuilder<bool>(
+                //Espera la inicialización de firebase
+                future: checkLoginStatus(),
+
+                builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                  if (snapshot.data == false) {
+                    // ignore: prefer_const_constructors
+                    return PaginaBienvenida(); // Si el usuario no ha iniciado sesión se muestra página de bienvenida
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container(
+                        color: Colors.red,
+                        child:
+                            const Center(child: CircularProgressIndicator()));
+                  }
+                  // ignore: prefer_const_constructors
+                  return AppPrincipal(); // Si el usuario ha iniciado sesión se muestra la página principal de la app
+                },
+              ));
+        });
   }
 }
